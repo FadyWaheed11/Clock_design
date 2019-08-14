b45d25dbc94b0b6c9ec65a29470fcaa116e81aa1
FadyWaheed11 <fadywaheed10@gmail.com>
2019-08-14 16:01:07 +0200

    all files added

---
+++ lib/screens/first_screens.dart
@@ -0,0 +1,77 @@
+import 'package:clock/clock/clock.dart';
+import 'package:flutter/material.dart';
+class FirstTab extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        SizedBox(
+          height: 85,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Clock()
+        ),
+        SizedBox(
+          height: 85,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Alarm Time",
+                  style: TextStyle(
+                    color: Color(0xffff0863),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 1.3
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Text(
+                  "06:12PM",
+                  style: TextStyle(
+                    color: Color(0xff2d386b),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+
+              ],
+            ),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Walk up in",
+                  style: TextStyle(
+                    color: Color(0xffff0863),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 1.3
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Text(
+                  "08:AM",
+                  style: TextStyle(
+                    color: Color(0xff2d386b),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+  
+}
